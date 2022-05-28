eb052de096ba5bb302bcf3e638fd11aa763d3675
Guilherme Tavares <guilherme.tsilva@gmail.com>
2022-05-28 18:51:26 -0300

    feat(gateway): add server config
    * feat(gateway): add server config
    * Merge branch 'main' of origin into feature/gateway
    * feat(gateway): improve docker-compose config

--- gateway/src/main/java/com/github/sozinhos/ecommerce/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/github/sozinhos/ecommerce/gateway/GatewayApplication.java
@@ -2,8 +2,10 @@ package com.github.sozinhos.ecommerce.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {

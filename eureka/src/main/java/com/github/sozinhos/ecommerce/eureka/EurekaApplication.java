8a8029aeec6e803f1327e26a15d491b2b87d3869
Guilherme Tavares <guilherme.tsilva@gmail.com>
2022-05-28 18:07:03 -0300

    feat(gateway): add server config

--- eureka/src/main/java/com/github/sozinhos/ecommerce/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/github/sozinhos/ecommerce/eureka/EurekaApplication.java
@@ -2,6 +2,7 @@ package com.github.sozinhos.ecommerce.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 public class EurekaApplication {

8b8fe733b934a9aba716161a7417de1b541abbbc
Guilherme Tavares <guilherme.tsilva@gmail.com>
2022-05-28 18:16:33 -0300

    Merge branch 'main' of origin into feature/gateway

--- eureka/src/main/java/com/github/sozinhos/ecommerce/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/github/sozinhos/ecommerce/eureka/EurekaApplication.java
@@ -2,9 +2,10 @@ package com.github.sozinhos.ecommerce.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
 	public static void main(String[] args) {

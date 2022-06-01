2e90e91049c7fe2dc81e4793da81c1911b343af7
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-05-31 22:09:11 -0300

    entities

---
+++ orders/src/main/java/com/github/sozinhos/ecommerce/orders/entities/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.github.sozinhos.ecommerce.orders.entities;
+
+public enum OrderStatus {
+    CART,
+    PENDING,
+    SUCCESS,
+    ERROR,
+    CANCELED;
+}
---
+++ orders/src/main/java/com/github/sozinhos/ecommerce/orders/entities/Product.java
@@ -0,0 +1,17 @@
+package com.github.sozinhos.ecommerce.orders.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+    private Integer amount;
+}

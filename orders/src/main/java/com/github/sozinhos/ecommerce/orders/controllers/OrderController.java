5710c664998cd18888152f9f507ef1f1c86e2e77
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-06-01 12:50:15 -0300

    orders controller

---
+++ orders/src/main/java/com/github/sozinhos/ecommerce/orders/controllers/OrderController.java
@@ -0,0 +1,44 @@
+package com.github.sozinhos.ecommerce.orders.controllers;
+
+import com.github.sozinhos.ecommerce.orders.entities.Order;
+import com.github.sozinhos.ecommerce.orders.services.OrderService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.create(order);
+    }
+
+    @GetMapping("/{id}")
+    public Order retrieveOrder(@PathVariable String id) {
+        return orderService.findById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public Order updateOrder(@PathVariable String id, @RequestBody Order order) {
+        return orderService.findByIdAndUpdate(id, order);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable String id) {
+        orderService.findByIdAndDelete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
